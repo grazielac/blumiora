8a2b599313a0278465954c7456119e661956549d
grazielac <cpgraziela@gmail.com>
2025-10-27 21:52:28 +0000

    Add background color

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,24 +1,33 @@
 "use client";
-import Link from 'next/link';
-import Image from 'next/image';
-import blumiora from '../public/blumiora.png';
-import { BsSearch, BsBell, BsChatDots } from 'react-icons/bs';
-
+import Image from "next/image";
+import blumiora from "../public/blumiora.png";
+import { BsSearch, BsBell, BsChatDots } from "react-icons/bs";
+import { UserButton } from "@clerk/nextjs";
 
 function NavBar() {
   return (
-    <nav className='flex gap-6'>
-        <Image src={blumiora} alt="Blumiora logo" width={120} height={100} />
-        <BsSearch size={24}/>
-        <BsChatDots size={24} />
-        <BsBell size={24} />
-        <Link href="" className=""></Link>
-        <Link href="" className=""></Link>
-      
+    <nav className="flex gap-6 items-center justify-between px-6 py-3 border-b border-border bg-white">
+      <div className="flex items-center gap-3">
+        <Image
+          src={blumiora}
+          alt="Blumiora logo"
+          width={120}
+          height={100}
+          priority
+        />
+      </div>
+      <div className="relative flex items-center gap-5 text-gray-700">
+        <BsSearch size={22} className="absolute left-3 top-2.5 text-gray-400 cursor-pointer" />
+            {/* <input type="text" placeholder="Search..." className="pl-10 pr-3 py-3 rounded-full border border-border bg-"></input> */}
+        <BsChatDots size={22} className="cursor-pointer" />
+        <BsBell size={22} className="cursor-pointer" />
+      </div>
+
+      <div className="flex items-center">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
-
-
+export default NavBar;
