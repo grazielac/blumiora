d465e587b7dd5ddeb75c67f4a211a0dfb5654928
grazielac <cpgraziela@gmail.com>
2025-10-27 20:29:16 +0000

    Add Blumiora logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Link from 'next/link';
+import Image from 'next/image';
+import blumiora from '../public/blumiora.png';
 
-import React from 'react'
 
 function NavBar() {
   return (
     <nav className='flex gap-6'>
+        <Image src={blumiora} alt="Blumiora logo" width={120} height={100} />
         <Link href="/" className="text-primary hover:underline">Home</Link>
         <Link href="" className=""></Link>
         <Link href="" className=""></Link>
