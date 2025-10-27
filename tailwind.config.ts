f842e0bbfd333ac97e764382362d15e00dc1d1ce
grazielac <cpgraziela@gmail.com>
2025-10-27 15:42:27 +0000

    Add tailwind.config.ts with custom color palette

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6A2E35",
+        accentGray: "#D8CFC4",
+        accentApricot: "#f5e9d3",
+        pagebg: "#FDFCF8",
+        card: "#FFFFFF",
+        text: "#333333",
+        text2: "#7d7d7d",
+        border: "#e3e2df",
+        alert: "#F26868",
+      },
+    },
+  },
+  plugins: [],
+};
