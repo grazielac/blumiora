914d2a51c766e13e8f669edede163f76270e9550
grazielac <cpgraziela@gmail.com>
2025-10-28 09:33:59 +0000

    Tailwind not workinggit add .

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+
+
+function Hero() {
+  return (
+    <div className="w-full h-screen bg-gradient-to-b from-amber-100 to-rose-50">
+      <h1 className="text-2xl">For the ones who create, feel and inspire.</h1>
+      <h3>A space for creatives to connect, grow, and bloom.</h3>
+    </div>
+  );
+}
+
+export default Hero;
