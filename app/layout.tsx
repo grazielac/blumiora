d75093aa4d9e23d7aead441aaab1f605560f8b99
grazielac <cpgraziela@gmail.com>
2025-10-28 09:53:56 +0000

    ClerkProvider wraps entire app (layout.tsx), NavBar conditionally render buttons (consumes Clerk state

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import NavBar from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,20 +34,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <NavBar />
           {children}
         </body>
       </html>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,12 +2,11 @@
 import Image from "next/image";
 import blumiora from "../public/blumiora.png";
 import { BsSearch, BsBell, BsChatDots } from "react-icons/bs";
-import { UserButton } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 function NavBar() {
   return (
-    <nav className="flex gap-6 items-center justify-between px-6 py-3 border-b border-border">
-      <div className="flex items-center gap-3">
+    <header className="flex items-center justify-between p-4 h-16">
         <Image
           src={blumiora}
           alt="Blumiora logo"
@@ -15,10 +14,10 @@ function NavBar() {
           height={100}
           priority
         />
-      </div>
+     
       <div className="relative flex items-center gap-5">
         <BsSearch size={22} className=" text-gray-500 cursor-pointer" />
-            {/* <input type="text" placeholder="Search..." className="pl-10 pr-3 py-3 rounded-full border border-border bg-"></input> */}
+        {/* <input type="text" placeholder="Search..." className="pl-10 pr-3 py-3 rounded-full border border-border bg-"></input> */}
         <BsChatDots size={22} className="cursor-pointer text-gray-500" />
         <BsBell size={22} className="cursor-pointer text-gray-500" />
       </div>
@@ -26,7 +25,7 @@ function NavBar() {
       <div className="flex items-center">
         <UserButton afterSignOutUrl="/" />
       </div>
-    </nav>
+    </header>
   );
 }
 
