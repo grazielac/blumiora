e4bcd2dab09d54068a269412d0bab5f4f8c1de08
grazielac <cpgraziela@gmail.com>
2025-10-28 10:04:36 +0000

    Add Sign In / Sign Up buttons to NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,28 +2,47 @@
 import Image from "next/image";
 import blumiora from "../public/blumiora.png";
 import { BsSearch, BsBell, BsChatDots } from "react-icons/bs";
-import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 function NavBar() {
   return (
     <header className="flex items-center justify-between p-4 h-16">
-        <Image
-          src={blumiora}
-          alt="Blumiora logo"
-          width={120}
-          height={100}
-          priority
-        />
-     
-      <div className="relative flex items-center gap-5">
-        <BsSearch size={22} className=" text-gray-500 cursor-pointer" />
-        {/* <input type="text" placeholder="Search..." className="pl-10 pr-3 py-3 rounded-full border border-border bg-"></input> */}
-        <BsChatDots size={22} className="cursor-pointer text-gray-500" />
-        <BsBell size={22} className="cursor-pointer text-gray-500" />
-      </div>
+      <Image
+        src={blumiora}
+        alt="Blumiora logo"
+        width={120}
+        height={100}
+        priority
+      />
+      {/* left side */}
+      <div className="flex items-center gap-4">
+        <SignedOut>
+          <SignInButton>
+            <button className="bg-gray-200 px-4 py-2 rounded-full text-sm sm:text-base">
+              {" "}
+              Sign In
+            </button>
+          </SignInButton>
+          <SignUpButton>
+            <button className="bg-accentGray">Sign Up</button>
+          </SignUpButton>
+        </SignedOut>
 
-      <div className="flex items-center">
-        <UserButton afterSignOutUrl="/" />
+        <SignedIn>
+          <nav className="flex gap-4">
+            <BsSearch size={22} className=" text-gray-500 cursor-pointer" />
+            {/* <input type="text" placeholder="Search..." className="pl-10 pr-3 py-3 rounded-full border border-border bg-"></input> */}
+            <BsChatDots size={22} className="cursor-pointer text-gray-500" />
+            <BsBell size={22} className="cursor-pointer text-gray-500" />
+          </nav>
+          <UserButton />
+        </SignedIn>
       </div>
     </header>
   );
