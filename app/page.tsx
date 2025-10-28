b1dfc705fcb80ac94d6595db56e78017f77df5fc
grazielac <cpgraziela@gmail.com>
2025-10-28 19:25:41 +0000

    Render Hero only when <SignedOut>

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 // import NavBar from "@/components/NavBar";
+import { SignedOut } from "@clerk/nextjs";
 import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <>
       {/* <NavBar /> */}
-      <Hero />
+      <SignedOut>
+        <Hero />
+      </SignedOut>
     </>
   );
 }
