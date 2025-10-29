b0db4c59df6de89631d8226bedecc7bdf712cd5d
grazielac <cpgraziela@gmail.com>
2025-10-29 19:49:19 +0000

    Wrap JoinButton inside SignUpButton

--- components/JoinButton.tsx
+++ components/JoinButton.tsx
@@ -1,9 +1,12 @@
+import { SignUpButton } from '@clerk/nextjs'
 import React from 'react'
 
 function JoinButton() {
   return (
     <div>
+      <SignUpButton>
       <button className='shadow-md bg-primary font-bold text-white rounded-full px-4 py-4 hover:bg-accentGray hover:text-text text-sm'>JOIN BLUMIORA</button>
+      </SignUpButton>
     </div>
   )
 }
