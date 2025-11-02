c8d0cbb5c3716324556226d60b9beab043d03049
grazielac <cpgraziela@gmail.com>
2025-11-02 17:45:00 +0000

    Add NotFound page

---
+++ app/posts/[id]/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested post</p>
+      <Link href="/">Return to the homepage</Link>
+    </div>
+  );
+}
+
+export default NotFound;
