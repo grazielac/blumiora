47e951fe688f0c88fdcc8a77c70b19ee9a93afb0
grazielac <cpgraziela@gmail.com>
2025-11-03 12:10:59 +0000

    Add publishable key on Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en" suppressHydrationWarning>
-        <body className={`${cormorant.variable} ${inter.variable} antialiased bg-pagebg min-h-screen`}>
-          
+        <body
+          className={`${cormorant.variable} ${inter.variable} antialiased bg-pagebg min-h-screen`}
+        >
           {children}
           <Footer />
         </body>
